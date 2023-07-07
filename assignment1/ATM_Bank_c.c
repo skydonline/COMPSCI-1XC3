b3fcda68153d2c2ea2801b8c7085cf884d5c849e
Sky Deng <sky.deng.online@outlook.com>
2023-07-07 13:30:01 -0400

    started part c

--- assignment1/ATM_Bank_c.c
+++ assignment1/ATM_Bank_c.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
+
 
 int main()
 {
@@ -7,21 +9,40 @@ int main()
     float balance = 0;
     float deposit = 0;
     float withdraw = 0;
-    float interest;
+    float interest = -1;
     int years;
+    int num_accounts;
+
+    printf("Enter the number of accounts: ");
+    scanf("%d", &num_accounts);
 
 
-    printf("Enter interest rate (enter percentage without the symbol): ");
-    scanf("%f", &interest);
 
+    while (1) {
+        printf("Enter interest rate: ");
+
+        if (scanf("%f", &interest) == 1) {
+            if (interest < 0 || interest > 100) {
+                printf("Error: Invalid interest rate entered. Enter a number between 0 and 100.\n");
+            } 
+            else {
+                break;
+            }
+        } 
+
+        else {
+            printf("Error: Invalid input format. Enter a valid interest rate without any symbols.\n");
+            
+            while (getchar() != '\n')
+                continue;
+        }
+    }
 
     while (choice != 0)
     {
 
-
-        printf("Select an operation:\n 1. Deposit\n 2. Withdraw\n 3. Check Balance\n 4. Future Balance Using Interest Rate\n 0. exit\n");
+        printf("Select an operation:\n 1. Deposit\n 2. Withdraw\n 3. Check Balance\n 4. Future Balance Using Interest Rate\n 5. Switch Account\n 6. Display All Balances\n 0. exit\n");
         scanf("%d", &choice);
-
 
         switch (choice)
         {

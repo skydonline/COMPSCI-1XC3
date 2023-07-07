be1069f4bac7c3a7b01e5caee3d28cfdc8bc7c11
Sky Deng <sky.deng.online@outlook.com>
2023-07-07 08:07:11 -0400

    part a assignment 1, reorganized files

---
+++ assignment 1/ATM_Bank_a.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int choice;
+    float balance = 0;
+    float deposit = 0;
+    float withdraw = 0;
+    
+
+    while (choice != 0) {
+
+        printf("Select an operation:\n 1. Deposit\n 2. Withdraw\n 3. Check Balance\n 0. exit\n");
+        scanf("%d", &choice);
+
+
+
+        switch (choice) {
+
+            case 1:
+                printf("Enter the amount you would like to deposit:\n");
+                scanf("%f", &deposit);
+                balance += deposit;
+                break;
+            
+            case 2:
+                printf("Enter the amount you would like to deposit:\n");
+                scanf("%f", &withdraw);
+                balance -= withdraw;
+                break;
+
+            case 3:
+                printf("Current balance: $%.2f\n", balance);
+                break;
+
+            case 0:
+                break;
+    }
+    }
+}

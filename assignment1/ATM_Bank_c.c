6fe46b3a3500535977e8d1cf890e946a746d1526
Sky Deng <sky.deng.online@outlook.com>
2023-07-12 10:47:44 -0400

    finished error handling

--- assignment1/ATM_Bank_c.c
+++ assignment1/ATM_Bank_c.c
@@ -2,6 +2,13 @@
 #include <math.h>
 #include <stdbool.h>
 
+// Made a clear input buffer function to clean up code, since it is used multiple times in the code
+void clearInputBuffer() {
+    while (getchar() != '\n') {
+        continue;
+    }
+}
+
 int main()
 {
     // Create variables
@@ -23,16 +30,17 @@ int main()
         if (scanf("%d", &num_accounts) == 1 && num_accounts > 0 && num_accounts <= 10) {
                 break;
         } 
-
+        // Prints error message and clears input buffer
         else {
             printf("Error: Invalid input. Enter a valid number of accounts, an integer greater than 0, without any symbols.\n");
-            
-            // Clear input buffer
-            while (getchar() != '\n') {
-                continue;
+            clearInputBuffer();
             }
         }
-    }
+
+
+
+    // Clear input buffer incase user inputs invalid input that is left in input buffer
+    clearInputBuffer();
 
 
 
@@ -44,16 +52,18 @@ int main()
         if (scanf("%f", &interest) == 1 && interest <= 100 && interest >= 0) {
                 break;
         } 
-
+        // Prints error message and clears input buffer
         else {
             printf("Error: Invalid input. Enter a valid interest rate between 0 and 100, without any symbols.\n");
-            
-            // Clear input buffer
-            while (getchar() != '\n') {
-                continue;
-            }
+            clearInputBuffer();
         }
     }
+
+
+
+    // Clear input buffer incase user inputs invalid input that is left in input buffer
+    clearInputBuffer();
+
 
 
     // While loop prompts user for their desired operation
@@ -62,20 +72,44 @@ int main()
         
         // Checks if the input is a valid operation
         if (scanf("%d", &choice) == 1 && choice >= 0 && choice <= 6) {
+
+            // Clear input buffer incase user inputs invalid input that is left in input buffer
+            clearInputBuffer();
+
             switch (choice)
             {
             // Deposit
             case 1:
-                printf("Enter the amount you would like to deposit: ");
-                scanf("%f", &deposit);
-                balance += deposit;
+                while (1) {
+                    // Checks if input is a valid deposit
+                    printf("Enter the amount you would like to deposit: ");
+                    if (scanf("%f", &deposit) == 1 && deposit >= 0) {
+                        balance += deposit;
+                        break;
+                    } 
+                    // Prints error message and clears input buffer
+                    else {
+                        printf("Error: Invalid input. Enter a valid deposit, a number greater than 0 without any symbols.\n");
+                        clearInputBuffer();
+                    }
+                }
                 break;
 
             // Withdraw
             case 2:
-                printf("Enter the amount you would like to withdraw: ");
-                scanf("%f", &withdraw);
-                balance -= withdraw;
+                while (1) {
+                    // Checks if input is a valid withdrawl
+                    printf("Enter the amount you would like to withdraw: ");
+                    if (scanf("%f", &withdraw) == 1 && withdraw >= 0 && withdraw <= balance) {
+                        balance -= withdraw;
+                        break;
+                    }
+                    // Prints error message and clears input buffer
+                    else {
+                        printf("Error: Invalid input. Enter a valid withdrawl, a number greater than 0 and less than your total balance without any symbols.\n");
+                        clearInputBuffer();
+                    }
+                }
                 break;
 
             // Prints current balance
@@ -85,9 +119,19 @@ int main()
 
             // Prints future balance based on given interest rate and years
             case 4:
-                printf("Enter the amount of years: ");
-                scanf("%d", &years);
-                printf("Future Balance: %.2f\n", balance*pow((1+interest/100),years));
+                while (1) {
+                    // Checks if input is a valid number of years
+                    printf("Enter the amount of years: ");
+                    if (scanf("%d", &years) == 1 && years >= 0) {
+                        printf("Future Balance: %.2f\n", balance*pow((1+interest/100),years));
+                        break;
+                    }
+                    // Prints error message and clears input buffer
+                    else {
+                        printf("Error: Invalid input. Enter a valid number of years greater than 0 without any symbols.\n");
+                        clearInputBuffer();
+                    }
+                }
                 break;
 
             case 0:
@@ -95,16 +139,22 @@ int main()
             }
 
         } else {
-            printf("Error: Invalid input. Enter a number between 0 and 6, without any symbols.\n");
-            // Clear input buffer
-            while (getchar() != '\n') {
-                continue;
-            }
+            // Prints error message and clears input buffer
+            printf("Error: Invalid input. Enter a valid operation number between 0 and 6, without any symbols.\n");
+            clearInputBuffer();
         }
         
         // Exits program
         if (choice == 0) {
             break;
+        }
+
+        // Clear input buffer incase user inputs invalid input that is left in input buffer
+        // Note: if the user just checked the balance, there will be nothing in the input buffer
+        // Therefore, it is not necessary to clear the input buffer, and this needs to be checked
+        // Otherwise the function will not continue
+        if (choice != 3) {
+            clearInputBuffer();
         }
     }
 }
